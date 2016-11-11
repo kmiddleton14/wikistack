8a77e23f32ec08bec70672a364a576bddf31b5e2
Sophia Bok <sophiatca@gmail.com>
2016-11-11 17:59:22 -0500

    some of sequelize

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var morgan = require('morgan');
 var nunjucks = require('nunjucks');
 var bodyParser = require('body-parser');
 
-
+var models = require('./models');
 
 app.engine('html',nunjucks.render);
 app.set('view engine','html');
@@ -22,3 +22,14 @@ app.use(express.static(path.join(__dirname,'/public')));
 var server = app.listen(1111, function(){
 	console.log('listeing on port 1111');
 })
+
+models.User.sync({})
+.then(function () {
+    return models.Page.sync({})
+})
+.then(function () {
+    server.listen(3001, function () {
+        console.log('Server is listening on port 3001!');
+    });
+})
+.catch(console.error);
